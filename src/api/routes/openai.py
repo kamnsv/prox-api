54f4980398536cc78b54a8222207d8d99889d7b4
kamnsv <kamnev111@mail.ru>
2024-10-09 23:30:39 +0300

    Fix image type

--- src/api/routes/openai.py
+++ src/api/routes/openai.py
@@ -31,14 +31,17 @@ class OpenAI(API):
                 })
 
             if image:
+                if not image.content_type.startswith('image/'):
+                    raise HTTPException(status_code=400, detail="Uploaded file is not an image.")
+                
                 image_data = await image.read()
-                image_decode = base64.b64encode(image_data).decode('utf-8')
+                image_base64 = base64.b64encode(image_data).decode('utf-8')
 
                 content.append({
                     "type": "image_url",
-                    "image_url": f"data:image/jpeg;base64,{image_decode}"
+                    "image_url": f"data:{image.content_type};base64,{image_base64}"
                 })
-
+            
             if not len(content):
                 raise HTTPException(status_code=400, detail="If you need content to work, please enter context or upload an image")
             
