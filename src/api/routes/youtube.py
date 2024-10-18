5fc32d7915a4ce1dac9ade8292784d1a098ae7ce
Sergei Kamnev <kamnev111@mail.ru>
2024-10-18 08:01:22 +0300

    Added working script

--- src/api/routes/youtube.py
+++ src/api/routes/youtube.py
@@ -2,11 +2,13 @@ from youtubesearchpython import VideosSearch
 from fastapi import APIRouter
 from fastapi import Query
 from typing import Union
+import os
 
 from ..api import API
-
+from mods.prewyoutube import download_video, split_video, create_preview, extract_youtube_id
 class Youtube(API):
-
+    cache_video = 'data/youtube_videocache'
+    os.makedirs(cache_video)
     cache = {}
 
     def set_routes(self, api: APIRouter):
@@ -28,3 +30,12 @@ class Youtube(API):
             if name:
                 return {name: video_links}
             return video_links
+        
+        @api.get('prew')
+        async def create_preview(video_url: str, parts: int=5, sec: int=2):
+            name = extract_youtube_id(video_url)
+            if not os.path.isfile( f'{self.cache_video}/{name}.mp4'):
+                download_video(video_url, f'{self.cache_video}/{name}.mp4')
+                split_video('video.mp4', parts)  # делим на 5 частей
+                create_preview([f'{self.cache_video}/{name}_{i}.webm' for i in range(5)], 2)  # берем по 2 секунды из каждой части
+            return 
